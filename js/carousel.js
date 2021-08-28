25d2209327361600c91e89679a2b0a1970081414
Effie Bara <effiebara@gmail.com>
2021-08-28 18:59:09 +0300

    added game

--- js/carousel.js
+++ js/carousel.js
@@ -19,3 +19,15 @@ function initSlideShow(slideshow) {
 
 	}, time);
 }
+
+var character =
+document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if(character.classList != "animate"){
+    character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
